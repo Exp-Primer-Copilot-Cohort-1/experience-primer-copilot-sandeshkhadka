2adf4c249d310e4c69f1ed552efc754a626f7aa4
sandeshkhadka <84300784+sandeshkhadka@users.noreply.github.com>
2023-12-22 06:35:38 +0000

    Copilot Third Commit

---
+++ comment.js
@@ -0,0 +1,13 @@
+// Create web server
+
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+router.post('/', commentController.commentPost);
+router.get('/list', commentController.commentList);
+router.get('/list/:id', commentController.commentListById);
+router.put('/:id', commentController.commentUpdate);
+router.delete('/:id', commentController.commentDelete);
+
+module.exports = router;
